0eeb25e637cbd6be088e71830360ab8027667c1f
Marko Vukosav <marko.vukosav85@gmail.com>
2021-05-29 22:05:07 +0200

    setting up Firebase

--- app/src/main/java/com/example/snapchat/MainActivity.kt
+++ app/src/main/java/com/example/snapchat/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.snapchat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import com.google.firebase.auth.FirebaseAuth
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
+
+    var emailEditText: EditText?= null
+    var passwordEditText: EditText?= null
+    val mAuth = FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        emailEditText = findViewById(R.id.emailEditText)
+        passwordEditText = findViewById(R.id.passwordEditText)
+
+        if (mAuth.currentUser != null){
+            logIn()
+        }
+    }
+
+    fun goClicked(view: View){
+
+    }
+
+    fun logIn(){
+        //move to next Activity
+
     }
 }
