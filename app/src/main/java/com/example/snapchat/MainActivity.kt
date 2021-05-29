f8302b5374245991f962311a6029ad7d999f1f79
Marko Vukosav <marko.vukosav85@gmail.com>
2021-05-29 22:21:05 +0200

    setting up sign up/in

--- app/src/main/java/com/example/snapchat/MainActivity.kt
+++ app/src/main/java/com/example/snapchat/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.snapchat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import kotlin.math.log
 
@@ -26,11 +28,33 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun goClicked(view: View){
+        //Check if we can log in the user
+        mAuth.signInWithEmailAndPassword( emailEditText?.text.toString(), passwordEditText?.text.toString())
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                logIn()
+                } else {
+                    //Sign up the user
+                mAuth.createUserWithEmailAndPassword(emailEditText?.text.toString(), passwordEditText?.text.toString())
+                    .addOnCompleteListener(this){task ->
+                        if (task.isSuccessful){
+                            //Add to database
+                            logIn()
+                        }else{
+                            Toast.makeText(this,"Login Failed. Try Again.", Toast.LENGTH_SHORT).show()
+
+                        }
+
+                    }
+
+                }
+            }
 
     }
 
     fun logIn(){
         //move to next Activity
-
+        val intent = Intent(this,SnapsActivity::class.java)
+        startActivity(intent)
     }
 }
